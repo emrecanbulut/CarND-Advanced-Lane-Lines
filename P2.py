954a89407c389bb0cebd532597303d624101e209
Emre Can Bulut <ebulut@lely.com>
2019-06-22 22:56:44 +0300

    README.md added. Example outputs added.

--- P2.py
+++ P2.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 import glob
 import matplotlib.pyplot as plt
+import os
+from moviepy.editor import VideoFileClip
 
 
 def calibrateMyCamera():
@@ -33,7 +35,7 @@ def calibrateMyCamera():
 
     return ret, mtx, dist, rvecs, tvecs
 
-def colorAndGradient(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
+def colorAndGradient(img, s_thresh=(90, 180), sx_thresh=(20, 100)):
     img = np.copy(img)
     # Convert to HLS color space and separate the V channel
     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
@@ -59,28 +61,22 @@ def colorAndGradient(img, s_thresh=(170, 255), sx_thresh=(20, 100)):
     combined_binary[(s_binary == 1) | (sxbinary == 1)] = 1
     return combined_binary
 
-def readAndUndistortImages(mtx, dist):
-    images = glob.glob('test_images/*.jpg')
-    output_images = []
-    for fname in images:
-        img = cv2.imread(fname)
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        undist = cv2.undistort(img, mtx, dist, None, mtx)
-        #showBeforeAndAfter(img, undist)
-        output_images.append(undist)
-    return output_images
+def readAndCvt(fname):
+    img = cv2.imread(fname)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    return img
 
 def showBeforeAndAfter(before_img, after_img):
     f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
     f.tight_layout()
-    ax1.imshow(before_img, cmap='gray')
+    ax1.imshow(before_img)#, cmap='gray')
     ax1.set_title('Before Image', fontsize=50)
-    ax2.imshow(after_img, cmap='gray')
+    ax2.imshow(after_img)#, cmap='gray')
     ax2.set_title('After Image', fontsize=50)
     plt.subplots_adjust(left=0.1, right=0.9, top=0.9, bottom=0.1)
     mng = plt.get_current_fig_manager()
     mng.window.state('zoomed')
-    plt.show()
+    plt.savefig('test_images_output\warped_straight_lines')
 
 def drawPolygon(img):
     output = np.copy(img)
@@ -91,15 +87,17 @@ def drawPolygon(img):
     return output
 
 def perspectiveTransform(img):
+    global transMatrix
     imshape = img.shape[1::-1]
     src = np.float32([(imshape[0]*0.156,imshape[1]),(imshape[0]*0.469, imshape[1]*0.62), (imshape[0]*0.529, imshape[1]*0.62), (imshape[0]*0.862,imshape[1])])
     dst = np.float32([(imshape[0]*0.3,imshape[1]*0.98),(imshape[0]*0.3, imshape[1]*0.01), (imshape[0]*0.7, imshape[1]*0.01), (imshape[0]*0.7,imshape[1]*0.98)])
     #dst = np.float32([(imshape[0]*0.3,imshape[1]),(imshape[0]*0.1, 0), (imshape[0]*0.9, 0), (imshape[0]*0.7,imshape[1])])
     # Given src and dst points, calculate the perspective transform matrix
-    M = cv2.getPerspectiveTransform(src, dst)
+    transMatrix = cv2.getPerspectiveTransform(src, dst)
     # Warp the image using OpenCV warpPerspective()
-    warped = cv2.warpPerspective(img, M, imshape, flags=cv2.INTER_LINEAR)
-    return warped
+    unwarped = cv2.warpPerspective(img, transMatrix, imshape, flags=cv2.INTER_LINEAR)
+    return unwarped
+
 def find_lane_pixels(binary_warped):
     # Take a histogram of the bottom half of the image
     histogram = np.sum(binary_warped[binary_warped.shape[0]//2:,:], axis=0)
@@ -110,7 +108,7 @@ def find_lane_pixels(binary_warped):
     # margin to be ignored on the horizontal
     ignored_margin = int(histogram.shape[0]*0.15)
     midpoint = np.int(histogram.shape[0]//2)
-    temp = histogram[ignored_margin:midpoint]
+
     leftx_base = np.argmax(histogram[ignored_margin:midpoint]) + ignored_margin
     rightx_base = np.argmax(histogram[midpoint:(histogram.shape[0]-ignored_margin)]) + midpoint
 
@@ -146,12 +144,6 @@ def find_lane_pixels(binary_warped):
         win_xright_low = rightx_current - margin
         win_xright_high = rightx_current + margin
         
-        # Draw the windows on the visualization image
-        cv2.rectangle(out_img,(win_xleft_low,win_y_low),
-        (win_xleft_high,win_y_high),(0,255,0), 2) 
-        cv2.rectangle(out_img,(win_xright_low,win_y_low),
-        (win_xright_high,win_y_high),(0,255,0), 2) 
-        
         # Identify the nonzero pixels in x and y within the window #
         good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & 
         (nonzerox >= win_xleft_low) &  (nonzerox < win_xleft_high)).nonzero()[0]
@@ -184,49 +176,191 @@ def find_lane_pixels(binary_warped):
 
     return leftx, lefty, rightx, righty, out_img
 
-def fit_polynomial(binary_warped):
-    # Find our lane pixels first
-    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
-
-    ### TO-DO: Fit a second order polynomial to each using `np.polyfit` ###
+def fit_poly(img_shape, leftx, lefty, rightx, righty):
+     ### TO-DO: Fit a second order polynomial to each with np.polyfit() ###
+    global lane_mid_point
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-
     # Generate x and y values for plotting
-    ploty = np.linspace(0, binary_warped.shape[0]-1, binary_warped.shape[0] )
-    try:
-        left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-        right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-    except TypeError:
-        # Avoids an error if `left` and `right_fit` are still none or incorrect
-        print('The function failed to fit a line!')
-        left_fitx = 1*ploty**2 + 1*ploty
-        right_fitx = 1*ploty**2 + 1*ploty
+    ploty = np.linspace(0, img_shape[0]-1, img_shape[0])
+    ### TO-DO: Calc both polynomials using ploty, left_fit and right_fit ###
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+    lane_mid_point = (left_fitx[img_shape[0]-1] + right_fitx[img_shape[0]-1])/2
+    return left_fitx, right_fitx, ploty, left_fit, right_fit
+
+def search_around_poly(binary_warped):
+    # HYPERPARAMETER
+    # Choose the width of the margin around the previous polynomial to search
+    # The quiz grader expects 100 here, but feel free to tune on your own!
+    margin = 50
+
+    # Grab activated pixels
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    
+    ### TO-DO: Set the area of search based on activated x-values ###
+    ### within the +/- margin of our polynomial function ###
+    ### Hint: consider the window areas for the similarly named variables ###
+    ### in the previous quiz, but change the windows to our new search area ###
+    global prev_left_fit, prev_right_fit, left_curverad, right_curverad
+
+    left_lane_inds = ((nonzerox > (prev_left_fit[0]*(nonzeroy**2) + prev_left_fit[1]*nonzeroy + 
+                    prev_left_fit[2] - margin)) & (nonzerox < (prev_left_fit[0]*(nonzeroy**2) + 
+                    prev_left_fit[1]*nonzeroy + prev_left_fit[2] + margin)))
+    right_lane_inds = ((nonzerox > (prev_right_fit[0]*(nonzeroy**2) + prev_right_fit[1]*nonzeroy + 
+                    prev_right_fit[2] - margin)) & (nonzerox < (prev_right_fit[0]*(nonzeroy**2) + 
+                    prev_right_fit[1]*nonzeroy + prev_right_fit[2] + margin)))
+    
+    # Again, extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    # Fit new polynomials
+    # update previous fits for the next iteration
+    left_fitx, right_fitx, ploty, prev_left_fit, prev_right_fit = fit_poly(binary_warped.shape, leftx, lefty, rightx, righty)
+    y_eval = np.max(ploty)
+    left_curverad, right_curverad = calculateCurvature(prev_left_fit, prev_right_fit, y_eval)
 
     ## Visualization ##
-    # Colors in the left and right lane regions
-    out_img[lefty, leftx] = [255, 0, 0]
-    out_img[righty, rightx] = [0, 0, 255]
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped))*255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
 
-    # Plots the left and right polynomials on the lane lines
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+
+    left_line_window = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    right_line_window = np.array([np.flipud(np.transpose(np.vstack([right_fitx, 
+                              ploty])))])
+    lane_pts = np.hstack((left_line_window, right_line_window))
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([lane_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    
+    # Plot the polynomial lines onto the image
+    '''
     plt.plot(left_fitx, ploty, color='yellow')
     plt.plot(right_fitx, ploty, color='yellow')
+    '''
+    ## End visualization steps ##   
+    return result, window_img
+
+def calculateCurvature(left_fit_cr, right_fit_cr, y_eval):
+    ym_per_pix = 500/720 # meters per pixel in y dimension
+    left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)**1.5) / np.absolute(2*left_fit_cr[0])
+    right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
+    return int(left_curverad), int(right_curverad)
     
-    plt.xlim(0, 1280)
-    plt.ylim(720, 0)
-    plt.imshow(out_img)
-    plt.show()
+def work_on_video(filename='project_video.mp4'):
+    white_output = 'output_videos/'+ filename
+    ## To speed up the testing process you may want to try your pipeline on a shorter subclip of the video
+    ## To do so add .subclip(start_second,end_second) to the end of the line below
+    ## Where start_second and end_second are integer values representing the start and end of the subclip
+    ## You may also uncomment the following line for a subclip of the first 5 seconds
+    ##clip1 = VideoFileClip("test_videos/solidWhiteRight.mp4").subclip(0,5)
+    clip1 = VideoFileClip(filename)#.subclip(0,5) #solidWhiteRight.mp4")
+    white_clip = clip1.fl_image(video_processor) #NOTE: this function expects color images!!
+    white_clip.write_videofile(white_output, audio=False)
+
+def video_processor(image):
+    processed_img = process_image(image)
+    unwarped = transformBackToOriginal(processed_img)
+    result = cv2.addWeighted(image, 1, unwarped, 0.8, 0)
+    cv2.putText(
+        img=result,
+        text='Curvature radius: '+str((right_curverad+left_curverad)/2),
+        org=(0, 50),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=1,
+        color=(255, 255, 255))
+    
+    offset = vehiclePosition(lane_mid_point)
+    if offset < 0:
+        offset = -offset
+        msg = 'm right of the lane center'
+    else:
+        msg = 'm left of the lane center'
+    msg = str(offset)+msg
+
+    cv2.putText(
+        img=result,
+        text='Vehicle position: '+msg,
+        org=(0, 100),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+        fontScale=1,
+        color=(255, 255, 255))
+    return result
+
+def vehiclePosition(lane_midpoint_x):
+    xm_per_pixel = 3.7 / 500.0 
+    frame_midpoint_x = 639 #(1280/2)
+    pixel_difference = lane_midpoint_x - frame_midpoint_x
+    return pixel_difference*xm_per_pixel
+
+def work_on_images():
+    global isFirstFrame
+    images = glob.glob('test_images/*.jpg')
+    for file in images:
+        isFirstFrame = True
+        image = readAndCvt(file)
+        processed_img = process_image(image)
+        unwarped = transformBackToOriginal(processed_img)
+        result = cv2.addWeighted(image, 1, unwarped, 0.8, 0)
+        showBeforeAndAfter(result, result)
+
+def process_image(image):
+    # NOTE: The output you return should be a color image (3 channel) for processing video below
+    # TODO: put your pipeline here,
+    # you should return the final output (image where lines are drawn on lanes)
+    global mtx, dist, isFirstFrame, prev_left_fit, prev_right_fit
+    undist = cv2.undistort(image, mtx, dist, None, mtx)
+    combined_binary = colorAndGradient(undist)
+    #saveImage(combined_binary, 'Combined Binary Image', 'combined_binary_output')
+    #roi_drawn = drawPolygon(undist)
+    #roi_drawn = cv2.cvtColor(roi_drawn,cv2.COLOR_BGR2RGB)
+    binary_warped = perspectiveTransform(combined_binary)
+    #showBeforeAndAfter(roi_drawn, binary_warped)
+    if(isFirstFrame):
+        leftx, lefty, rightx, righty, _ = find_lane_pixels(binary_warped)
+        _, _, _, prev_left_fit, prev_right_fit = fit_poly(binary_warped.shape, leftx, lefty, rightx, righty)
+        isFirstFrame = False
+    out_img, _= search_around_poly(binary_warped)
     return out_img
 
-if __name__ == '__main__':
-    ret, mtx, dist, rvecs, tvecs = calibrateMyCamera()
-    undist_images = readAndUndistortImages(mtx, dist)
-    
+def saveImage(img, title, filename):
+    cv2.imwrite('test_images_output/'+filename+'.png',img)
+    '''
+    plt.imshow(img)# cmap='gray')
+    plt.title(title)    
+    if not os.path.exists('test_images_output'):
+        os.makedirs('test_images_output')
+    plt.savefig('test_images_output/'+filename)
+    '''
+def transformBackToOriginal(processed_img):
+    global transMatrix
+    invTransMatrix = np.linalg.inv(transMatrix)
+    imshape = processed_img.shape[1::-1]
+    warped = cv2.warpPerspective(processed_img, invTransMatrix, imshape, flags=cv2.INTER_LINEAR)
+    return warped
 
-    for image in undist_images:
-        combined_binary = colorAndGradient(image)
-#        roi_drawn = drawPolygon(combined_binary)
-        binary_warped = perspectiveTransform(combined_binary)
-#        showBeforeAndAfter(combined_binary, binary_warped)
-        out_img = fit_polynomial(binary_warped)
-#        showBeforeAndAfter(binary_warped, out_img)
+ret, mtx, dist, rvecs, tvecs = calibrateMyCamera()
+
+transMatrix = None
+isFirstFrame = True
+prev_left_fit = None
+prev_right_fit = None
+left_curverad = None
+right_curverad = None
+vehicle_pos = None
+lane_mid_point = None
+
+if __name__ == '__main__':
+    work_on_video()
+    #work_on_images()
--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-## Advanced Lane Finding
-[![Udacity - Self-Driving Car NanoDegree](https://s3.amazonaws.com/udacity-sdc/github/shield-carnd.svg)](http://www.udacity.com/drive)
-![Lanes Image](./examples/example_output.jpg)
-
-In this project, your goal is to write a software pipeline to identify the lane boundaries in a video, but the main output or product we want you to create is a detailed writeup of the project.  Check out the [writeup template](https://github.com/udacity/CarND-Advanced-Lane-Lines/blob/master/writeup_template.md) for this project and use it as a starting point for creating your own writeup.  
-
-Creating a great writeup:
----
-A great writeup should include the rubric points as well as your description of how you addressed each point.  You should include a detailed description of the code used in each step (with line-number references and code snippets where necessary), and links to other supporting documents or external references.  You should include images in your writeup to demonstrate how your code works with examples.  
-
-All that said, please be concise!  We're not looking for you to write a book here, just a brief description of how you passed each rubric point, and references to the relevant code :). 
-
-You're not required to use markdown for your writeup.  If you use another method please just submit a pdf of your writeup.
-
-The Project
----
+**Advanced Lane Finding Project**
 
 The goals / steps of this project are the following:
 
@@ -26,14 +11,120 @@ The goals / steps of this project are the following:
 * Warp the detected lane boundaries back onto the original image.
 * Output visual display of the lane boundaries and numerical estimation of lane curvature and vehicle position.
 
-The images for camera calibration are stored in the folder called `camera_cal`.  The images in `test_images` are for testing your pipeline on single frames.  If you want to extract more test images from the videos, you can simply use an image writing method like `cv2.imwrite()`, i.e., you can read the video in frame by frame as usual, and for frames you want to save for later you can write to an image file.  
+[//]: # (Image References)
 
-To help the reviewer examine your work, please save examples of the output from each stage of your pipeline in the folder called `output_images`, and include a description in your writeup for the project of what each image shows.    The video called `project_video.mp4` is the video your pipeline should work well on.  
+[image1]: ./test_images_output/undistort_compared.png "Undistorted"
+[image2]: ./test_images_output/undistorted_output.png "Road Transformed"
+[image3]: ./test_images_output/combined_binary_output.png "Binary Example"
+[image4]: ./test_images_output/warped_straight_lines.png "Warp Example"
+[image5]: ./test_images_output/colored_lane_marked.png "Fit Visual"
+[image6]: ./test_images_output/example_output.png "Output"
+[video1]: ./output_videos/project_video.mp4 "Video"
 
-The `challenge_video.mp4` video is an extra (and optional) challenge for you if you want to test your pipeline under somewhat trickier conditions.  The `harder_challenge.mp4` video is another optional challenge and is brutal!
+## [Rubric](https://review.udacity.com/#!/rubrics/571/view) Points
 
-If you're feeling ambitious (again, totally optional though), don't stop there!  We encourage you to go out and take video of your own, calibrate your camera and show us how you would implement this project from scratch!
+### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
 
-## How to write a README
-A well written README file can enhance your project and portfolio.  Develop your abilities to create professional README files by completing [this free course](https://www.udacity.com/course/writing-readmes--ud777).
+---
 
+### Writeup / README
+
+#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one.  You can submit your writeup as markdown or pdf.  [Here](https://github.com/udacity/CarND-Advanced-Lane-Lines/blob/master/writeup_template.md) is a template writeup for this project you can use as a guide and a starting point.  
+
+You're reading it!
+
+### Camera Calibration
+
+#### 1. Briefly state how you computed the camera matrix and distortion coefficients. Provide an example of a distortion corrected calibration image.
+The code for this part can be found in `calibrateMyCamera()` method. First of all, I replicated some points of the chessboard 
+that represents the real world space. Then, I read all the calibration images and converted them to `GRAY` 
+before using the OpenCV method `findChessboardCorners(gray, (9,6),None)`. After each image where successfully found the corners, I added 
+replicated points to `objpoints` list and the corners found to `imgpoints` list. Afterwards, I used 
+`calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)` of OpenCV.
+
+
+![Undistorted comparison][image1]
+
+### Pipeline (single images)
+
+#### 1. Provide an example of a distortion-corrected image.
+
+----FILL
+To demonstrate this step, I will describe how I apply the distortion correction to one of the test images like this one:
+![alt text][image2]
+
+#### 2. Describe how (and identify where in your code) you used color transforms, gradients or other methods to create a thresholded binary image.  Provide an example of a binary image result.
+
+I used color transforms and gradient in combination to create a binary image. This part can be found in `colorAndGradient()` method. 
+I converted the color space into HLS and excluded the H channel. I used the L channel to take the derivative in x (sobel x) and I used 
+the S channel for color thresholds. Then combined the output of them in one frame. The output looks like this:
+![alt text][image3]
+
+#### 3. Describe how (and identify where in your code) you performed a perspective transform and provide an example of a transformed image.
+
+The code for this part can be found in `perspectiveTransform()` method. The method takes an image as input. I chose source(`src`) and 
+destination(`dst`) points. Then, I used `getPerspectiveTransform(src, dst)` of OpenCV to find the `transformationMatrix` and warp the image 
+using `warpPerspective` method of OpenCV.
+
+```python
+
+src = np.float32([
+	(imshape[0]*0.156, imshape[1]),
+	(imshape[0]*0.469, imshape[1]*0.62), 
+	(imshape[0]*0.529, imshape[1]*0.62), 
+	(imshape[0]*0.862, imshape[1])])
+dst = np.float32([
+	(imshape[0]*0.3, imshape[1]*0.98),
+	(imshape[0]*0.3, imshape[1]*0.01),
+	(imshape[0]*0.7, imshape[1]*0.01),
+	(imshape[0]*0.7, imshape[1]*0.98)])
+
+```
+
+This resulted in the following source and destination points:
+
+| Source        | Destination   | 
+|:-------------:|:-------------:| 
+| 199.68, 720   | 384, 705.6    | 
+| 600.32, 446.4 | 384, 7.2      |
+| 677.12, 446.4 | 896, 7.2      |
+| 1103.36, 720  | 896, 705.6    |
+
+I verified that my perspective transform was working as expected by drawing the `src` and `dst` points onto a test image and its warped counterpart to verify that the lines appear parallel in the warped image.
+
+![alt text][image4]
+
+#### 4. Describe how (and identify where in your code) you identified lane-line pixels and fit their positions with a polynomial?
+
+The part where I identified lane-line pixels can be found in `find_lane_pixels(binary_warped)`. Shortly, in the `binary_warped` input 
+image, I vertically checked where most of the nonzero values lied, and added their indices to lane-line pixels list.
+
+The part where I fit a 2nd order polynomial to the lane-line pixels can be found in `fit_poly()` method.
+
+![alt text][image5]
+
+#### 5. Describe how (and identify where in your code) you calculated the radius of curvature of the lane and the position of the vehicle with respect to center.
+
+I calculated the radius of curvature in `calculateCurvature()` method. And the vehicle position calculation was based on the assumption that 
+the camera was mounted exactly in the middle of the car. So, the pixel difference between midpoint of the lane and the midpoint of the frame 
+multiplied by pixel-per-kilometer constant results in the position offset.
+
+#### 6. Provide an example image of your result plotted back down onto the road such that the lane area is identified clearly.
+
+I implemented this step in `transformBackToOriginal` method using the inverse of `transformationMatrix`
+![alt text][image6]
+
+---
+
+### Pipeline (video)
+
+#### 1. Provide a link to your final video output.  Your pipeline should perform reasonably well on the entire project video (wobbly lines are ok but no catastrophic failures that would cause the car to drive off the road!).
+
+Here's a [link to my video result](./output_videos/project_video.mp4)
+
+
+### Discussion
+
+#### 1. Briefly discuss any problems / issues you faced in your implementation of this project.  Where will your pipeline likely fail?  What could you do to make it more robust?
+I haven't worked on the sanity of the lane lines that were detected. Therefore, once it detects some other line as the lane line, it will keep searching the lane line around the wrong line and 
+will almost always fail to find the correct one.
